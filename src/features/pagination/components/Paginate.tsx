227033768176b5bb75a3a048b35fea1649f0f601
maratdev0210 <maratforwork1@gmail.com>
2025-09-12 10:53:50 +0300

    pages are now showing based on the current page

--- src/features/pagination/components/Paginate.tsx
+++ src/features/pagination/components/Paginate.tsx
@@ -1,26 +1,39 @@
 import { useContext } from "react";
 import { PageContext } from "../../../context/blog/PageContext";
 
+// there can't be more than 5 pages displayed at the same time
+function displayPages(currentPage: number): number[] {
+  // special cases for first and last pages
+  if (currentPage == 1 || currentPage == 20) {
+    return [1, 2, 10, 19, 20];
+  }
+  if (currentPage == 2) {
+    return [1, 2, 3, 10, 20];
+  }
+  if (currentPage == 19) {
+    return [1, 10, 18, 19, 20];
+  }
+  return [1, currentPage - 1, currentPage, currentPage + 1, 20];
+}
+
 export default function Paginate({
-  totalPages,
   setPage,
 }: {
-  totalPages: number;
   setPage: React.Dispatch<React.SetStateAction<number>>;
 }) {
-  const pages: number[] = Array.from({ length: totalPages }, (_, i) => i + 1);
   const currentPage = useContext(PageContext);
+  const pages: number[] = displayPages(currentPage);
 
   return (
     <>
-      <div className="flex gap-1">
+      <div className="flex gap-4">
         {pages.map((page) => {
           return (
             <span
               onClick={() => {
                 setPage(page);
               }}
-              className={`border-1 pt-0.5 pr-2 pl-2 pb-0.5 text-gray-700 cursor-pointer ${
+              className={`border-gray-300  border-1 pt-0.5 pr-2 pl-2 pb-0.5 text-gray-700 cursor-pointer ${
                 page == currentPage ? "bg-blue-300" : ""
               }`}
             >
--- src/features/pagination/index.tsx
+++ src/features/pagination/index.tsx
@@ -6,8 +6,8 @@ export default function Pagination({
   setPage: React.Dispatch<React.SetStateAction<number>>;
 }) {
   return (
-    <div className="w-full flex justify-center">
-      <Paginate setPage={setPage} totalPages={20} />
+    <div className="w-full flex justify-center mt-8 mb-8">
+      <Paginate setPage={setPage} />
     </div>
   );
 }
